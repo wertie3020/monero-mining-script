8533891bfb2f0485cac43311521c8811fd4c8270
wertie <wertie@pop-os.localdomain>
2023-11-24 16:42:30 +0100

    fix start p2pool and xmrig

--- Startup.sh
+++ Startup.sh
@@ -34,21 +34,25 @@ fi
 if [ "$useP2Pool" = "yes" ]
 then
   # get latest p2pool software
+  p2pool_folder="p2pool"
   p2pool_browser_download_url=$(curl -s https://api.github.com/repos/SChernykh/p2pool/releases/latest | grep "\"browser_download_url\":.*-linux-x64.tar.gz" | cut -d : -f 2,3 | tr -d \")
   wget $p2pool_browser_download_url
   p2pool_name=$(curl -s https://api.github.com/repos/SChernykh/p2pool/releases/latest | grep "\"name\":.*-linux-x64.tar.gz" | cut -d : -f 2,3 | tr -d \" | tr -d [:space:] | tr -d ,)
-  tar -xf "./${p2pool_name}"
+  tar -xzf "./${p2pool_name}" -C "${p2pool_folder}" --strip-components=1
   rm ./$p2pool_name
-
+   
   # start p2pool
-  screen -dmS p2pool /bin/bash -c "sudo .$p2pool_folder/p2pool --host $monerod_ip --wallet $walletAddres"
+  screen -dmS p2pool /bin/bash -c "sudo ./$p2pool_folder/p2pool --host $monerod_ip --wallet $walletAddres"
 fi
 
+
 # get latest xmrig software
+xmrig_folder="xmrig"
 xmrig_browser_download_url=$(curl -s https://api.github.com/repos/xmrig/xmrig/releases/latest | grep "\"browser_download_url\":.*-linux-static-x64.tar.gz" | cut -d : -f 2,3 | tr -d \") 
 wget $xmrig_browser_download_url
 xmrig_name=$(curl -s https://api.github.com/repos/xmrig/xmrig/releases/latest | grep "\"name\":.*-linux-static-x64.tar.gz" | cut -d : -f 2,3 | tr -d \" | tr -d [:space:] | tr -d ,)
-tar -xf "./${xmrig_name}"
+tar -xzf "./${xmrig_name}" -C "${xmrig_folder}" --strip-components=1
 rm ./$xmrig_name
+
 # start xmrig
-screen -dmS xmrig /bin/bash -c "sudo .$xmrig_folder/xmrig -o $p2pool_ip:3333"
+screen -dmS xmrig /bin/bash -c "sudo ./$xmrig_folder/xmrig -o $p2pool_ip:3333"
