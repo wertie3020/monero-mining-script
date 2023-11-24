c329f4160b4f467e1b0002a9ddd891f7f140da76
wertie <wertie@pop-os.localdomain>
2023-11-24 14:38:30 +0100

    store input user to .env file

--- Setup.sh
+++ Setup.sh
@@ -25,7 +25,29 @@ function read_with_prompt {
   done
 }
 
-# Server name configuration
-echo ""
-read_with_prompt name "example"
+# add your wallet addres
+echo "Enter wallet addres. It's highly recommended to create a new wallet for mining because wallet addresses are public on p2pool. You have to use the primary wallet address for mining. Subaddresses and integrated addresses are not supported, just like with monerod solo mining."
+read_with_prompt walletAddres "wallet Addres"
+echo "walletAddres=\"$walletAddres\"" > .env
+
+# install p2pool
+echo "Install P2Pool? type yes/no (default yes)"
+read_with_prompt useP2Pool "use P2Pool" "yes"
+echo "useP2Pool=\"$useP2Pool\"" >> .env
+
+# install Monerod
+echo "Install Monerod? type yes/no (default yes)"
+read_with_prompt UseMonerod "use Monerod" "yes"
+echo "UseMonerod=\"$UseMonerod\"" >> .env
+
+# add Monerod ip addres 
+echo "enter ip of Monerod. (default 127.0.0.1)"
+read_with_prompt monerod_ip "Monerod ip" "127.0.0.1"
+echo "monerod_ip=\"$monerod_ip\"" >> .env
+
+# add p2pool ip addres
+echo "enter ip of p2pool (default 127.0.0.1)"
+read_with_prompt p2pool_ip "p2pool ip" "127.0.0.1"
+echo "p2pool_ip=\"$p2pool_ip\"" >> .env
+
 
